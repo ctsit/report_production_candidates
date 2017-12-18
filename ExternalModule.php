08dca1cb6751c3c44ce790f7330d11ed1c757c8c
Dileep <rajputd@ufl.edu>
2017-12-18 16:13:43 -0500

    Add method to check if project_stats table exists

--- ExternalModule.php
+++ ExternalModule.php
@@ -4,7 +4,25 @@ namespace ReportProductionCandidatesModule\ExternalModule;
 use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
 
+define("TABLE_NAME", "redcap_project_stats");
+
 class ExternalModule extends AbstractExternalModule {
+
+
+  private function check_stats_table_exists() {
+
+    $result = ExternalModules::query("SHOW TABLES LIKE '" . TABLE_NAME  . "'");
+
+    if(!$result) {
+      throw new Exception("cannot access database.");
+    }
+
+    if($result->num_rows) {
+      return true;
+    }
+
+    return false;
+  }
 
 }
 
